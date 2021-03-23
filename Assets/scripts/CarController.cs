79d0ec27336b2155bda6bc300e247de163de5bcb
xp7353 <80015324+xp7353@users.noreply.github.com>
2021-03-23 11:58:55 +0900

    스와프카

---
+++ Assets/scripts/CarController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    float speed = 0.0f;
+    Vector2 startPos;
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            startPos = Input.mousePosition;
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            Vector2 endPos = Input.mousePosition;
+            float swipeLength = (endPos.x - startPos.x);
+            this.speed = swipeLength / 500.0f;
+            GetComponent<AudioSource>().Play();
+        }
+        transform.Translate(this.speed, 0, 0);
+        this.speed *= 0.98f;
+
+    }
+}
---
+++ Assets/scripts/GameDirector.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameDirector : MonoBehaviour
+{
+    GameObject car; GameObject flag; GameObject distance;
+    void Start()
+    {
+        this.car = GameObject.Find("car"); 
+        this.flag = GameObject.Find("flag"); 
+        this.distance = GameObject.Find("Distance");
+    }
+    void Update()
+    {
+        float length = this.flag.transform.position.x - this.car.transform.position.x;
+        if (length > 0.5f)
+            this.distance.GetComponent<Text>().text = "목표 지점까지 " + length.ToString("F2") + "m";
+        else if (length <= 0.5f && length >= -0.5f)
+            this.distance.GetComponent<Text>().text = "youwin";
+        else
+            this.distance.GetComponent<Text>().text = "GAMEOVER";
+
+    }
+}
